8577a947391e1a8a640a0d1ac12063be84704f16
shotaro gotanda <g.sho1500@gmail.com>
2017-06-19 18:08:03 +0900

    EvalErrでも続けるように修正

--- main.ml
+++ main.ml
@@ -1,16 +1,20 @@
 open Syntax
 open Eval
 
-let rec read_eval_print env =
+let rec read_eval_print env  =
   print_string "# ";
   flush stdout;
   let cmd = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let (id, newenv, v) = eval_command env cmd in
-  (Printf.printf "%s = " id;
+  try let (id, newenv, v) = eval_command env cmd in
+  (Printf.printf "%s : " id;
+   print_string " = ";
    print_value v;
    print_newline ();
-   read_eval_print newenv)
+   read_eval_print newenv
+   )
+  with (EvalErr str) -> print_string str;print_newline ();(read_eval_print env)
 
 let initial_env = empty_env
+let initial_tyenv = empty_env
 
 let _ = read_eval_print initial_env
